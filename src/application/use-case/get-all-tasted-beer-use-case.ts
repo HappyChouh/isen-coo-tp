fc53bd9476a8451451bd704e67345631def5bd60
RonanBalusson <balusson.ronan16@gmail.com>
2023-10-13 17:05:11 +0200

    modification

---
+++ src/application/use-case/get-all-tasted-beer-use-case.ts
@@ -0,0 +1,5 @@
+import { TastedBeer } from "../../domain/entity/tasted-beer";
+import { BeerRepository } from "../../domain/repository/beer-repository";
+import { TastedBeerRepository } from "../../domain/repository/tasted-beer-repository";
+
+export type AddTastedBeerUseCaseDependencies = {};
